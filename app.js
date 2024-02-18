96c8f3a22185eb5ac3352d9957a374701de2167d
Mykyta-Ushakow <nilitosushak@gmail.com>
2024-02-19 00:56:39 +0200

    Getting mdb to connect

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
+import "./configs/dotenvConfig.js";
 import contactsRouter from "./routes/contactsRouter.js";
+import serverConfig from "./configs/serverConfig.js";
 
 const app = express();
+
+// MONGOOSE CONNECTION ====================
+
+mongoose
+	.connect(serverConfig.mongoUrl)
+	.then(() => {
+		console.log("Mongo DB connected..");
+	})
+	.catch((err) => {
+		console.log(err);
+		process.exit(1);
+	});
+
+// MIDDLEWARES ===========================
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+// ROUTER ===========================
+
 app.use("/api/contacts", contactsRouter);
+
+// ERRORS ===========================
 
 app.use((_, res) => {
 	res.status(404).json({ message: "Route not found" });
@@ -21,6 +42,10 @@ app.use((err, req, res, next) => {
 	res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-	console.log("Server is running. Use our API on port: 3000");
+// SERVER INIT ===============================
+
+const { port } = serverConfig;
+
+app.listen(port, () => {
+	console.log(`Server is running. Use our API on port: ${port}`);
 });
---
+++ configs/dotenvConfig.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+	path: path.resolve(
+		process.cwd(),
+		"envs",
+		`${process.env.NODE_ENV ?? "development"}.env`
+	),
+});
